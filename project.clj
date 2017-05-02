c9c637ec7bea0b0b64e037b923e93a521e09dacf
Vadim Liventsev <liventsevvadim@gmail.com>
2017-05-02 22:44:40 +0300

    Tinkering

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject toocold "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "An incredibly simple API to look up how good a city's climate is"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [metosin/compojure-api "1.1.10"]]
+  :min-lein-version "2.0.0"
   :ring {:handler toocold.handler/app}
   :main toocold.handler
-  :uberjar-name "server.jar"
   :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]
                    :plugins [[lein-ring "0.10.0"]]}})
